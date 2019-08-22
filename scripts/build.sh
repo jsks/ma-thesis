e1064bd9924a56e33e7905d2da739e47b4cbb489
Joshua Krusell <js.shirin@gmail.com>
2019-08-22 19:09:25 +0200

    Genericise build/run scripts for podman

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,10 @@
 
 set -e
 
-docker build -t jsks/conflict_onset:latest .
-docker push jsks/conflict_onset
+if which podman 2>&1 >/dev/null; then
+    podman build -t docker.io/jsks/conflict_onset .
+    podman push docker.io/jsks/conflict_onset
+else
+    docker build -t jsks/conflict_onset:latest .
+    docker push jsks/conflict_onset
+fi
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-docker run -d -v $PWD:/proj -v /mnt/data/posteriors:/proj/posteriors \
-       jsks/conflict_onset make $@
-container=$(docker ps -l --format "{{.Names}}")
+set -e
+
+which podman 2>&1 >/dev/null && CMD=podman || CMD=docker
+echo "Running with $CMD"
+
+# No arrays in posix sh...
+mount_opts="-v $PWD:/proj"
+if [ -d /mnt/data/posteriors ]; then
+    echo "Mounting posterior map from /mnt/data"
+    mount_opts="$mount_opts -v /mnt/data/posteriors:/proj/posteriors"
+fi
+
+$CMD run -d $mount_opts jsks/conflict_onset make $@
+container=$($CMD ps -l --format "{{.Names}}")
 
 echo "Launched: $container"
-docker logs -f $container
+$CMD logs -f $container
