e13b270a404cb20ee0dfd04b15c9349f78b0c45b
Joshua Krusell <js.shirin@gmail.com>
2019-09-01 23:35:53 +0200

    Add arg to set quantile colnames in post_summarise

--- R/thesis.utils/R/post.R
+++ R/thesis.utils/R/post.R
@@ -5,8 +5,12 @@
 #' draws or extracted directly from a `stanfit` object.
 #'
 #' @param x NumericMatrix or `stanfit` object
-#' @param names Optional CharacterVector identifying the columns from
+#' @param par.names Optional CharacterVector identifying the columns from
 #'     the stanfit matrix.
+#' @param col.names Optional named CharacterVector replacing the
+#'     default column names specified by the vector names with the
+#'     given vector elements. Note: default column names are according
+#'     to [quantile()], *i.e.* as percentage intervals.
 #' @param probs Quantile probabilities to use when summarising `x`
 #' @param ... Arguments passed to methods
 #'
@@ -17,22 +21,39 @@
 #' m <- matrix(c(rnorm(100), rnorm(100, 10, 5)), 100, 2)
 #' post_summarise(m)
 #'
+#' post_summarise(m, probs = c(0.2, 0.8),
+#'                col.names = c(`20%` = "codelow", `80%` = "codehigh"))
+#'
 #' @export
-post_summarise <- function(x, names = NULL,
+post_summarise <- function(x,
+                           par.names = NULL,
+                           col.names = NULL,
                            probs = c(0.025, 0.16, 0.5, 0.84, 0.975), ...)
     UseMethod("post_summarise")
 
 #' @describeIn post_summarise Method for a NumericMatrix extracted
 #'     from `stanfit` object
 #' @export
-post_summarise.matrix <- function(x, names = NULL,
+post_summarise.matrix <- function(x,
+                                  par.names = NULL,
+                                  col.names = NULL,
                                   probs = c(0.025, 0.16, 0.5, 0.84, 0.975), ...) {
     if (anyNA(x))
         stop("NA's in posterior matrix")
 
     df <- apply(x, 2, stats::quantile, probs = probs) %>% t %>% as.data.frame
     rownames(df) <- NULL
-    df$par <- if (is.null(names)) colnames(x) else names
+
+    df$parameter <- if (is.null(par.names)) colnames(x) else par.names
+
+    # Remember kids, safety first.
+    if (!is.null(col.names)) {
+        if (is.null(names(col.names)))
+            stop("Missing match labels for `col.names`")
+
+        v <- col.names[match(colnames(df), names(col.names))]
+        colnames(df) <- ifelse(is.na(v), colnames(df), v)
+    }
 
     df
 }
@@ -41,12 +62,15 @@ post_summarise.matrix <- function(x, names = NULL,
 #'
 #' @describeIn post_summarise Method for stanfit objects
 #' @export
-post_summarise.stanfit <- function(x, names = NULL,
-                                   probs = c(0.025, 0.16, 0.5, 0.84, 0.975), pars = NULL,
+post_summarise.stanfit <- function(x,
+                                   par.names = NULL,
+                                   col.names = NULL,
+                                   probs = c(0.025, 0.16, 0.5, 0.84, 0.975),
+                                   pars = NULL,
                                    ...) {
     if (is.null(pars))
         stop("Expected at least one parameter to extract from stanfit")
 
     m <- as.matrix(x, pars = pars)
-    post_summarise.matrix(m, names, probs)
+    post_summarise.matrix(m, par.names, col.names, probs)
 }
--- R/thesis.utils/tests/testthat/test_post.R
+++ R/thesis.utils/tests/testthat/test_post.R
@@ -1,19 +1,27 @@
 test_that("post_summarise", {
     # This is a pretty contrived example
     m <- matrix(1:100, 100, 1)
-    out <- data.frame(`2.5%` = 3.475,
-                      `16%` = 16.84,
-                      `50%` = 50.5,
-                      `84%` = 84.16,
-                      `97.5%` = 97.525,
-                      check.names = F)
+    y <- data.frame(`2.5%` = 3.475,
+                    `16%` = 16.84,
+                    `50%` = 50.5,
+                    `84%` = 84.16,
+                    `97.5%` = 97.525,
+                    check.names = F)
 
-    expect_equal(post_summarise(m), out)
+    expect_equal(post_summarise(m), y)
 
-    out$par <- "A"
-    expect_equal(post_summarise(m, names = "A"), out)
+    colnames(y)[c(1, 4)] <- c("hello", "goodbye")
+    y$parameter <- "A"
+    out <- post_summarise(m, par.names = "A",
+                          col.names = c(`2.5%` = "hello", `84%` = "goodbye"))
 
-    out <- data.frame(`10%` = 10.9, `90%` = 90.1, check.names = F)
-    expect_equal(post_summarise(m, probs = c(.10, .9)), out)
+    expect_equal(out, y)
+
+    y <- data.frame(`10%` = 10.9, `90%` = 90.1, check.names = F)
+
+    expect_equal(post_summarise(m, probs = c(.10, .9)), y)
+
     expect_error(post_summarise(matrix()))
+    expect_error(post_summarise(m, col.names = letters[1:5]))
+    expect_error(post_summarise({ m[1, ] <- NA; m }))
 })
