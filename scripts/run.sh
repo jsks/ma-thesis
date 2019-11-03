7fc20ab54f971d134a893ae139d2d98cd1ed2765
Joshua Krusell <js.shirin@gmail.com>
2019-11-03 11:41:07 +0100

    Update run script
    `podman logs -f` does not EOF at container exit so instead just start
    the container in attached mode, for only stdout and stderr, and manually
    require a detach when running longer models, or just start everything in
    tmux as usual and detach from there.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,29 +1,24 @@
 #!/bin/sh
 #
-# CLI arguments are passed directly to make in the spawned
-# container. If the environmental variable CLEANUP is defined and set
-# to 1, then the container will be automatically removed following
-# completion.
+# Launches an attached container instance using either docker or
+# podman with CLI arguments passed directly to `make`. If the
+# environmental variable CLEANUP is defined and set to 1, then the
+# container will be automatically removed following completion.
 ###
 
 set -e
 
 which podman 2>&1 >/dev/null && CMD=podman || CMD=docker
-echo "Running with $CMD"
 
 # No arrays in posix sh...
-mount_opts="-v $PWD:/proj"
+mount_opts="-v $(git rev-parse --show-toplevel):/proj"
 if [ -d /mnt/data/posteriors ]; then
     echo "Mounting posterior map from /mnt/data"
     mount_opts="$mount_opts -v /mnt/data/posteriors:/proj/posteriors"
 fi
 
-$CMD run -d $mount_opts jsks/conflict_onset make $@
-container=$($CMD ps -l --format "{{.Names}}")
-
-echo "Launched: $container"
-$CMD logs -f $container
-
 if [ "${CLEANUP:-0}" = 1 ]; then
-    docker container rm $container
+    rm_opts="--rm"
 fi
+
+$CMD run -a stdout -a stderr $mount_opts $rm_opts jsks/conflict_onset make $@
